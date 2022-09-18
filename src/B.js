98584b902cb44638069c87889f54746bf65c000d
Anup Ray <rayanup95@gmail.com>
2022-09-18 20:03:07 +0530

    basics of ratatype without design

--- src/B.js
+++
@@ -1,8 +0,0 @@
-function B(algaKichhi){
-    return <div>
-        Name: {algaKichhi.buttonName.name} 
-        Roll: {algaKichhi.buttonName.roll}
-        {/* This is in B */}
-    </div>
-}
-export default B
--- src/Ratatype.js
+++ src/Ratatype.js
@@ -3,18 +3,54 @@ import './Ratacolor.css';
 
 function Ratatype() {
 
+    // const
     const paragraph = "this is first line"
+
+    //state
     const [color, setColor] = useState('color-green')
-    console.log(color)
+    const [speed, setSpeed] = useState(0)
+    const [startTime, setStartTime] = useState(0)
+    const [wrongInputCount, setWrongInputCount] = useState(0)
+    const [rightInputCount, setRightInputCount] = useState(0)
+    const [isFirstInput, setIsFirstInput] = useState(false)
+    const [myvalue, setMyvalue] = useState()
+
+    const inputChangeHandler = (event) => {
+        if (!isFirstInput) {
+            setIsFirstInput(true)
+            setStartTime(new Date())
+        }
+
+        const inputString = event.target.value
+        let inputLength = inputString.length
+
+
+
+        let isTypingCorrect = (paragraph.substring(0, inputLength) !== inputString) ? false : true;
+        isTypingCorrect ? setRightInputCount(rightInputCount + 1) : setWrongInputCount(wrongInputCount + 1)
+        isTypingCorrect ? setColor('color-green') : setColor('color-red');
+        isTypingCorrect && calculateSpeed(startTime, inputLength);
+        if (isTypingCorrect) {
+            setMyvalue(event.target.value)
+        }
+        
+
+
+    }
+    const calculateSpeed = (startTime, inputLength) => {
+        const currentTime = new Date()
+        const timeTaken = (currentTime - startTime) / 1000
+        setSpeed(parseInt((inputLength / timeTaken) * 12))
+    }
+
 
     return <div >
-      
-        {paragraph}<br></br>
-        <input type="text" className={color} onChange={(event) => {
-            const inputString = event.target.value
-            let inputLength = inputString.length
-            let temp =    (paragraph.substring(0, inputLength) !== inputString) ? setColor('color-red') : setColor('color-green')
-        }} />
+
+        {paragraph}<br></br><br></br>
+        <input type="text" className={color} value={myvalue} onChange={inputChangeHandler} />
+        <br></br><br></br>
+        speed: {speed} WPM<br></br><br></br>
+        accuracy : {(parseInt((rightInputCount) / (rightInputCount + wrongInputCount) * 100)) || 100}
     </div>
 }
 export default Ratatype
